1b3ab491b88f0af27ab67fdcc5a22ce07f2eb84a
Marek Ciesla <ciesla.marek@gmail.com>
2019-07-21 16:19:48 +0200

    Added event listeners for buttons

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,19 @@
-"use strict";
-
-// service worker registration - remove if you're not going to use it
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
-// place your code below
-
-
-console.log(`Hello world!`);
+const saveBtn = document.querySelector('.save-btn-js');
+const loadBtn = document.querySelector('.load-btn-js');
+const clearBtn = document.querySelector('.clear-btn-js');
+const removeBtn = document.querySelector('.remove-btn-js');
+const textArea = document.querySelector('.textarea-js')
+saveBtn.addEventListener('click', (e) => {
+  localStorage.setItem('textInput', textArea.value);
+})
+loadBtn.addEventListener('click', (e) => {
+  textArea.value = localStorage.getItem('textInput');
+})
+clearBtn.addEventListener('click', (e) => {
+  textArea.value = "";
+})
+removeBtn.addEventListener('click', (e) => {
+  localStorage.removeItem('textInput');
+})
 
 
